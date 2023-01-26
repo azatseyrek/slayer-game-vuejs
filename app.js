c106e45732ce3e82577f9ba50973b07209d6ccc5
azatseyrek <azatseyrek@hotmail.com>
2023-01-26 17:22:30 +0300

    attack function created

---
+++ app.js
@@ -0,0 +1,30 @@
+function getRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+const app = Vue.createApp({
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+      currentRound: 0,
+      winner: null,
+      logMessages: [],
+    };
+  },
+  methods: {
+    attackMonster() {
+      const attackValue = getRandomValue(5, 12);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      const attackValue = getRandomValue(8, 15);
+      this.playerHealth -= attackValue;
+    },
+  },
+  computed: {},
+  watch: {},
+});
+
+app.mount('#game');
