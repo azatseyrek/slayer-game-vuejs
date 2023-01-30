7489880e27c2d507f7af161e4c03d7fd1a3c1d6e
azatseyrek <azatseyrek@hotmail.com>
2023-01-30 10:51:59 +0300

    adding game log

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const app = Vue.createApp({
   },
   methods: {
     attackMonster() {
+      this.currentRound++;
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
@@ -22,9 +23,70 @@ const app = Vue.createApp({
       const attackValue = getRandomValue(8, 15);
       this.playerHealth -= attackValue;
     },
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    healPlayer() {
+      this.currentRound++;
+      const healValue = getRandomValue(8, 20);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healValue;
+      }
+      this.attackPlayer();
+    },
+    surrender() {
+      this.winner = 'monster';
+    },
+    startGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.currentRound = 0;
+      this.winner = null;
+      this.logMessages = [];
+    },
   },
-  computed: {},
-  watch: {},
+  computed: {
+    monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return {width: '0%'};
+      }
+      return {width: this.monsterHealth + '%'};
+    },
+    playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return {width: '0%'};
+      }
+      return {width: this.playerHealth + '%'};
+    },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    },
+  },
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        // A draw
+        this.winner = 'draw';
+      } else if (value <= 0) {
+        // Player lost
+        this.winner = 'monster';
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        // A draw
+        this.winner = 'draw';
+      } else if (value <= 0) {
+        // Monster lost
+        this.winner = 'player';
+      }
+    },
+  },
 });
 
 app.mount('#game');
